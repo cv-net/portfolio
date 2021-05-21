a757e3cd7995acd3a81c44adb5b97138c99b6a21
kabdellah <khalil.mktg@gmail.com>
2021-05-21 01:24:31 -0400

    Finished plastic bag props

--- src/PlasticBagProps.js
+++ src/PlasticBagProps.js
@@ -22,9 +22,7 @@ const PlasticBag = styled.div`
         width: 10%;
         display: inline;
     }
-    h2 {
-        text-align: center;
-    }
+
     h3 {
         padding-bottom: .5em;
     }
@@ -59,8 +57,8 @@ function PlasticBagProps() {
     This project is the product of an introductory course to data journalism, <a href='https://www.firstpythonnotebook.org/about/index.html' target='_blank' rel="noreferrer">First Python Notebook.</a>  
      First Python Notebook used data from the <a href='https://www.californiacivicdata.org/' target='_blank' rel="noreferrer">California Civic Data Coalition (CCDC)</a>, which cleans and 
     organizes publicly available political campaign finance data from the state of California (<a href='https://cal-access.sos.ca.gov/' target='_blank' rel="noreferrer">CAL-ACCESS</a>). 
-    The course focused on data from California's 2016 Ballot Propositions, interviewing the data and 
-    visualizing it with Python Jupyter Notebook, emphasizing the journalistic knowledge and 
+    The course focused on data from California's 2016 Ballot Propositions, interviewing the data, and 
+    visualizing it with Python Jupyter Notebook while emphasizing the journalistic knowledge and 
     sense needed to interpret the data responsibly. 
             </div>
             <div className='paragraph'>
@@ -78,46 +76,37 @@ function PlasticBagProps() {
     The visualizations below were created in Tableau, after geocoding the data based on the 
     ZIP codes provided in the CCDC dataset.            
             </div>
+                <hr />
+                <div className='paragraph'>
+        <span id='lede'>In 2016</span>, there were twin propositions on California's ballot, Propositions 65 and 67. 
+        These propositions were on the ballot because of a successful veto of SB-270, the plastic bag ban, 
+        which was signed into law two years earlier. Proposition 67 represented SB-270, and Proposition 65 the APBA's veto. 
+        The veto was petitioned by the American Progressive Bag Alliance (APBA), who disguised a clear conflict of interest with the plastic bag ban.
+                </div>
+                <div className='paragraph'>
+        Under SB-270, single-use plastic bags would be replaced with 
+        re-usable bags, which would be for sale to grocery store customers. Under Proposition 67, the revenue from the sale of those reusable bags would 
+        go back to grocery store owners, to cover their costs for manufacturing and procuring the bags, and educating customers
+        on sustainable shopping.
+                </div>
+                <div className='paragraph'>
+        The APBA presented Proposition 65 as a more sustainable version of Proposition 67 in its ballot narrative. 
+        65 said that the revenue from re-usable bag sales would go to an Environmental Protection and Enhancement Fund (EPEF). 
+                </div>
+                <div className='paragraph callout'>
+                        <em>
+                "The Wildlife Conservation Board would have utilized the fund for environmental protection and 
+                grants to environmental conservation organizations. Grants could have been used for drought mitigation; 
+                clean drinking water supplies; recycling; litter removal; wildlife habitat restoration; beach cleanup; 
+                and state, regional, and local parks." - Ballotpedia.com
+                        </em>
+                </div>
+
             <div className='paragraph'>
-    In California, there were twin propositions on the 2016 ballot, Propositions 65 and 67. 
-    Proposition 67 represented a vote on Senate Bill 270, which was a ban on single-use plastic 
-    carryout bags in grocery stores. Under SB-270, single-use plastic bags would be replaced with 
-    re-usable bags, which would be for sale to grocery store customers. 
-            </div>
-            <div className='paragraph'>
-    Proposition 65 presented as a more sustainable version of Proposition 67 in its ballot narrative. 
-    In actuality, both propositions were only on the ballot because Prop. 65 challenged to 
-    veto SB-270 altogether.
-            </div>
-            <div className='paragraph'>
-    It may have seemed, to an uninformed voter, that the main difference between Propositions 65 and 67 
-    was about where the revenue from selling re-usable bags would go.             
-            </div>
-            <div className='paragraph'>
-    Proposition 67 was a vote on the ban itself, and said that the revenue would go back to grocery 
-    store owners, to cover their costs for manufacturing and procuring the bags, and educating customers
-    on sustainable shopping. 
-            </div>
-            <div className='paragraph'>
-    Interviewing the campaign finance data provided by CCDC and reading the official ballot narratives 
-    made it clear that Proposition 65 and its plastic-producing backers had 
-    clandestine (however obvious) motivations.
-            </div>
-            <div className='paragraph'>
-    Proposition 65 said that the revenue from re-usable bag sales would go to an 
-    Environmental Protection and Enhancement Fund (EPEF). 
-            </div>
-            <div className='paragraph'>
-                <em>
-    "The Wildlife Conservation Board would have utilized the fund for environmental protection and 
-    grants to environmental conservation organizations. Grants could have been used for drought mitigation; 
-    clean drinking water supplies; recycling; litter removal; wildlife habitat restoration; beach cleanup; 
-    and state, regional, and local parks." - Ballotpedia.com
-                </em>
-            </div>
-            <div className='paragraph'>
+        It may have seemed, to an uninformed voter, that the main difference between Propositions 65 and 67 
+        was about where the revenue the bags would go.
     Though the EPEF seems a worthwhile cause, environmentalist support <a href='https://ballotpedia.org/California_Proposition_67,_Plastic_Bag_Ban_Veto_Referendum_(2016)#Support_for_.22yes.22_vote' target='_blank' rel="noreferrer">largely fell with Proposition 67</a>. 
-    That's a hint into the story of the twin propositions, which the campaign finance data will begin to 
+    That's a hint to the underlying motives of the APBA in vetoing Proposition 65, which the campaign finance data will begin to 
     clarify. 
             </div>
             <div className='paragraph'>
@@ -128,8 +117,8 @@ function PlasticBagProps() {
             <img src={figure1} alt='Figure 1'/>
             <p>Fig. 1, a screenshot from the Python notebook for this project. Available on Github after the jump.</p>
             <div className='paragraph APBApara'>
-    These 5 companies contributed to the one committee sponsoring the proposition, the American Progressive Bag Alliance.
-    Proposition 65 didn't have any contributions against it, probably because it wouldn't have been worth taking 
+    These 5 companies contributed to the one committee sponsoring the proposition, the American Progressive Bag Alliance. Proposition 65's supporters were listed as opposition to Proposition 67.
+    Proposition 65 didn't have any opposing contributions, probably because it wouldn't have been worth taking 
     focus away from Proposition 67.            
             </div>
             <img src={figure7} className='APBA' alt='Logo for the American Progressive Bag Alliance'/>
@@ -144,11 +133,6 @@ function PlasticBagProps() {
     would profit from selling paper bags in place of plastic bags. Prop. 65's redirection of funds from 
     grocery stores to the EPEF claimed to prevent profiteering, to keep the environmentalist effort pure. 
             </div>
-            <div className='paragraph'>
-    The opposing argument was that the revenue from selling paper bags would have covered the grocery stores'
-    costs for buying the bags and providing environmentally educational material to customers, which 
-    Prop. 67 mandated.
-            </div>
             <div className='paragraph callout'>
     Prop. 65 official ballot narrative, presented to voters in the booth
 
@@ -160,7 +144,7 @@ function PlasticBagProps() {
     Stop the sweetheart special interest deal... VOTE NO ON PROP 67. "
             </div>
             <div className='paragraph'>
-    Proposition 67 had many small contributors, largely from California residents.
+    In spite of this, Proposition 67 had many small contributors, largely from California residents.
             </div>
             <h3>Prop. 67 - REFERENDUM TO OVERTURN BAN ON SINGLE-USE PLASTIC BAGS.</h3>
             <img src={figure3} alt='Figure 3'/>
@@ -189,7 +173,7 @@ function PlasticBagProps() {
             </div>
             <h3>67 vs. 65</h3>
             <div className='paragraph'>
-    We find a story about the two propositions, and narratives about contributor motives on <a href='https://ballotpedia.org/California_Proposition_65,_Dedication_of_Revenue_from_Disposable_Bag_Sales_to_Wildlife_Conservation_Fund_(2016)' target='_blank' rel="noreferrer">Ballotpedia</a>.
+    We find the fine print of Proposition 65, something that makes a little more sense for a plastic bag company to support, on <a href='https://ballotpedia.org/California_Proposition_65,_Dedication_of_Revenue_from_Disposable_Bag_Sales_to_Wildlife_Conservation_Fund_(2016)' target='_blank' rel="noreferrer">Ballotpedia</a>.
             </div>
             <img src={figure6} alt='Figure 6'/>
             <p>Figure 6, from Ballotpedia.</p>
@@ -225,17 +209,18 @@ function PlasticBagProps() {
             <div className='paragraph'>
     In this story, a few plastic bag companies making up the APBA put their money together and created a 
     misleading proposition to sabotage the Prop. 67 plastic bag ban. That Prop. 65 was listed in opposition 
-    to Prop. 67, and that Prop. 65 was sponsored by plastic bag companies, says that 65 suggest was 
-    disingenuous in its framing. The APBA's proposition 65 claimed to be even more environmentalist 
+    to Prop. 67, and that Prop. 65 was sponsored by plastic bag companies, suggests that 65 was 
+    dishonest in its framing. The APBA's proposition 65 claimed to be even more environmentalist 
     than the initial plastic bag ban. In fact, Prop. 65 was as profit-driven and environmentally 
     destructive as it accused Prop. 67 of being. The APBA was the real 'sweetheart', trying to 
-    meet its own needs at the public's expense. 
+    meet its own industries needs at the expense of the environment. 
             </div>
             <div className='paragraph'>
-    The story of Propositions 67 and 65 is important because it's an example of political tricks at 
+    The story of Propositions 67 and 65 is important less because of plastic, and more because it's gross example of political tricks at 
     the state level. Politicians use disingenuous narratives and <a href='https://www.theverge.com/2020/12/29/22204976/section-230-senate-deal-stimulus-talks-checks' target='_blank' rel="noreferrer">use policies as bargaining chips for 
-    partisan aims</a>.
+    partisan aims</a>. These sorts of tricks have been somewhat normalized, but open civic data makes it possible to distinguish these stories and bring them back to voter discourse.
             </div>
+
         <div className='citations'>
         <h3>Citations:</h3>
         <ul>
